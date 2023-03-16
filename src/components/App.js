749d6228f092cac43bacb18535d98eab8e54c556
Suraj-Accio <suraj@acciojob.com>
2023-03-16 15:26:54 +0530

    submitting code-Thu Mar 16 2023 15:26:54 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
 import React from "react";
 import './../styles/App.css';
+import Form from "./Form";
+
+const fields = [
+  { name: "name", type: "text", label: "Name" },
+  { name: "email", type: "email", label: "Email" },
+  { name: "password", type: "password", label: "Password" }
+];
+
+const onSubmit = (formData) => {
+  console.log(formData);
+};
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  return <Form fields={fields} onSubmit={onSubmit} />;
+};
 
-export default App
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Form = ({ onSubmit, fields }) => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {fields.map((field) => (
+        <div key={field.name}>
+          <label htmlFor={field.name}>{field.label}</label>
+          <input
+            type={field.type}
+            id={field.name}
+            name={field.name}
+            value={formData[field.name] || ""}
+            onChange={handleChange}
+          />
+        </div>
+      ))}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
